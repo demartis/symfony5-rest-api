93bd8b311ca4f9b0260b2985a9679e0fd3f52282
Riccardo De Martis <riccardo@demartis.it>
2020-03-30 13:33:49 +0200

    cmd: composer require sensio/framework-extra-bundle
    same as: composer require annotations

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
