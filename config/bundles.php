37c46edac3641df464eacead6879ccf6bdca4f63
Riccardo De Martis <riccardo@demartis.it>
2020-04-01 19:58:19 +0200

    cmd: composer require symfony/orm-pack

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,6 @@ return [
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
