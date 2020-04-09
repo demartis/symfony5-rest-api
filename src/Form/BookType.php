93823224cb3b2b826034f74a90b46bb5459c98ac
Riccardo De Martis <riccardo@demartis.it>
2020-04-09 19:45:37 +0200

    cmd: bin/console make:form BookType

---
+++ src/Form/BookType.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Book;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BookType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('pages')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Book::class,
+        ]);
+    }
+}
