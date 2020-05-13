8e602e37002eb5d44f21aa945f608ea3f91f92dc
Riccardo De Martis <riccardo@demartis.it>
2020-05-13 11:07:20 +0200

    feat: more stable tag for fos_rest, added rest_routing
    new specs here:
    https://github.com/FriendsOfSymfony/FOSRestBundle/blob/master/UPGRADING-3.0.md

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Bazinga\Bundle\HateoasBundle\BazingaHateoasBundle::class => ['all' => true],
+    HandcraftedInTheAlps\RestRoutingBundle\RestRoutingBundle::class => ['all' => true],
 ];
