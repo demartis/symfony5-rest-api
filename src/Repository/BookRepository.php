8713184a1c39d4ca16f2e408a3d0dd59eb400613
Riccardo De Martis <riccardo@demartis.it>
2024-04-20 18:43:33 +0200

    fix: error 500 in v1_get_books
    (cherry picked from commit 4a8f7c0b16b3f7f861623bf282a315d99b0964de)

--- src/Repository/BookRepository.php
+++ src/Repository/BookRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Book;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method Book|null find($id, $lockMode = null, $lockVersion = null)
